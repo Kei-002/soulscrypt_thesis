a6e214340c1fd510fa37fd2b4a412401f1f17e30
Khen Vargas <khenchristian.vargas@tupe.edu.ph>
2023-11-04 09:51:42 +0800

    refactor: added default value for status

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('status');
+            $table->string('status')->default('unverified');
             $table->rememberToken();
             $table->timestamps();
         });
